5a81869323dcaaebc947c13c5fa100ed5fffaade
Elena Romanenko <murenec_elena@mail.ru>
2022-11-09 15:31:24 +0600

    added ex54

---
+++ ex54/Program.cs
@@ -0,0 +1,58 @@
+﻿// 54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int row = new Random().Next(2, 10);
+int column = new Random().Next(2, 10);
+Console.WriteLine($"Исходный массив {row}*{column}");
+
+int[,] matrix = new int[row, column]; // иниц-ция массива
+matrix = FillArray(matrix); // наполнение 
+PrintArray(matrix); // вывод на экран исходного массива
+BubbleSort(matrix); // м-д сортировки
+Console.WriteLine();
+PrintArray(matrix); // печать отсортированного массива
+
+int[,] FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] BubbleSort(int[,] matrix)
+{
+    int temp;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if(matrix[i, j] < matrix[i, k])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+return matrix;  
+}
+
